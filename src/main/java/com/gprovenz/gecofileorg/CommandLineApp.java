f6089ad48b34926401699888e3b592501d572264
Gaspare <39431368+gprovenz@users.noreply.github.com>
2025-06-14 17:09:26 +0200

    updated version

--- src/main/java/com/gprovenz/gecofileorg/CommandLineApp.java
+++ src/main/java/com/gprovenz/gecofileorg/CommandLineApp.java
@@ -14,7 +14,7 @@ import java.io.IOException;
  */
 public class CommandLineApp {
 
-    private static final String VERSION = "0.4.2-alpha";
+    private static final String VERSION = "1.0.0-beta";
 
     public static void main(String[] args ) throws IOException {
         System.out.println("Geco File Organizer v." + VERSION);
